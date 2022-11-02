2a4ebcdaa40c3ae7cd8a550b73cf2be2f18f1eee
MaxO <ostma@mail.ru>
2022-11-03 00:56:51 +0500

    Task66

---
+++ Task66/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов
+// в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int GetNumber(string message)
+{
+    Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine() ?? "");
+    return number;
+}
+
+void RecursionNum(int m, int n, int summ)
+{
+    summ = summ + n;
+    if (m >= n)
+    {
+        Console.WriteLine(summ);
+        return;
+    }
+    RecursionNum (m, n - 1, summ);
+}
+
+int m = GetNumber("Введите начальное число: ");
+int n = GetNumber("Введите конечное число: ");
+RecursionNum(m, n, 0);
